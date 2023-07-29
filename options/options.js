ab502fc803780a4802ddd413d4eb8f87b2088125
Crowdbotics <team@crowdbotics.com>
2023-07-29 16:07:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "munch-sandwiches-42513",
+  url: "https://munch-sandwiches-42513.botics.co",
+  api: "https://munch-sandwiches-42513.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
